723df1b5d7717324efa15dc0d98916aa4d6e738d
wmadarie <wikash.madarie@gmail.com>
2015-05-31 12:08:48 +0200

    test
    test

--- Cucumber/Features/step_definitions/add.rb
+++ Cucumber/Features/step_definitions/add.rb
@@ -13,7 +13,6 @@ end
 When(/^I press button add$/) do
 element = driver.find_element(:id, 'input')
 element.send_keys "TESTTESTTEST!"
-element.submit
 end
 
 Then(/^An item is added to the list$/) do
